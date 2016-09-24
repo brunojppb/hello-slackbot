5754478a16f4481792cc773e4177f0fe093f4e06
Bruno Paulino <brunojppb@gmail.com>
2016-09-24 12:56:18 -0300

    fix typo

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -20,7 +20,7 @@ class ReposController < ApplicationController
         text = "O repo #{repo} possui #{resp['stargazers_count']} Estrela(s)"
         render json: { text: text }
       else
-        render json: { text: 'Foi mal vei, não sei te responder ainda nossos 🖥 ainda estão aprendendo isso. 😉' }
+        render json: { text: 'Foi mal vei, não sei te responder ainda. nossos 🖥 ainda estão aprendendo isso. 😉' }
     end
   end
 
